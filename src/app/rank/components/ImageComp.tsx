51bd2b3e15c9b7893e5df6ce51daca30aa1fd158
CSH111 <seongho6022@naver.com>
2025-03-19 22:56:05 +0900

    feat: 기본이미지 색상표시

--- src/app/rank/components/ImageComp.tsx
+++ src/app/rank/components/ImageComp.tsx
@@ -1,11 +1,19 @@
 "use client";
+import styled from "@emotion/styled";
 import Image from "next/image";
 import { useState, ComponentProps } from "react";
+
+const DefaultImageBlock = styled.div<{ $width?: number; $height?: number }>`
+  background-color: #eaeaea;
+  width: ${(p) => (p.$width ? `${p.$width}px` : "100%")};
+  height: ${(p) => (p.$height ? `${p.$height}px` : "100%")};
+`;
 
 const ImageComp = (props: ComponentProps<typeof Image>) => {
   const [isError, setIsError] = useState(false);
   if (isError) {
-    return <Image {...props} src="/next.svg" />;
+    // return <Image {...props} src="/next.svg" />;
+    return <DefaultImageBlock $width={props.width as number} $height={props.width as number} />;
   }
   return (
     <Image
