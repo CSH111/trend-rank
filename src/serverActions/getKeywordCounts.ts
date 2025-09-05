9654aa582c417112e8f1e28e7495d80b867e3168
CSH111 <seongho6022@naver.com>
2025-09-06 02:59:17 +0900

    feat: 공고수 차트 순서 변경

--- src/serverActions/getKeywordCounts.ts
+++ src/serverActions/getKeywordCounts.ts
@@ -3,9 +3,10 @@ import { pr } from "../../PrismaClient";
 export default async function getKeywordCounts(keywordId: number) {
   const counts = await pr.keyword_counts.findMany({
     where: { refined_keyword_id: keywordId },
-    // orderBy: { created_at: "asc" },
+    orderBy: { id: "desc" },
     include: { report_dates: { where: { is_active: 1 } } },
     take: 4,
   });
+  counts?.reverse();
   return counts;
 }
